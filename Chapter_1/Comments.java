2c2e041566b54258c9889606ead482a938b72601
KenethMuyco-UCM <kennethmuyco03@gmail.com>
2025-08-29 16:18:07 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,19 @@
+public class Comments {
+
+    public static void main(String[] args) {
+        // This is a single-line comment
+
+        /*
+         * This is a multi-line comment.
+         * It can span multiple lines.
+         * It's useful for explaining larger blocks of code.
+         */
+
+        /**
+         * This is a Javadoc comment.
+         * It's used to document code for generating API documentation.
+         * @param args Command line arguments (not used in this example)
+         */
+        System.out.println("I love OOPROG!"); // This line prints a message to the console
+    }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,9 @@
+public class FavoriteSong {
+
+    public static void main(String[] args) {
+        System.out.println("Look at the stars");
+        System.out.println("Look how they shine for you");
+        System.out.println("And everything you do");
+        System.out.println("Yeah, they were all yellow");
+    }
+  }
