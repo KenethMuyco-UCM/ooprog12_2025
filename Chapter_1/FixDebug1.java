9e36c1c1bfa3c852978ac2e09f2b7c6ff26b0a94
KenethMuyco-UCM <kennethmuyco03@gmail.com>
2025-09-01 14:43:11 +0800

    Add files via upload

---
+++ Chapter_1/FixDebug1.java
@@ -0,0 +1,16 @@
+public class FixDebug1
+{
+    /* This program displays a greeting */
+    public static void main(String[] args)
+    {
+        System.out.println("HELLO WORLD!");
+    }
+}
+
+/*
+List of bugs fixed:
+1. Missing semicolon → added ; at the end of System.out.println("Hello World!");
+2. Class name syntax error → originally "FixDebug One", changed to "FixDebug1" (no space in class name).
+3. Extra "10" inside the class → removed invalid line.
+4. Misspelled "System" → corrected from "Systm.out.println" to "System.out.println".
+*/
